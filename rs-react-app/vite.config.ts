283517449c1e8df94466cd348fb079dad4536dbb
Denis Yeresko <denis.yeresko@gmail.com>
2025-07-19 19:26:13 +0300

    chore: add setup file in vite config

--- rs-react-app/vite.config.ts
+++ rs-react-app/vite.config.ts
@@ -6,6 +6,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   test: {
+    setupFiles: './test-utils/setup.ts',
     environment: 'jsdom',
     coverage: {
       provider: 'v8',
