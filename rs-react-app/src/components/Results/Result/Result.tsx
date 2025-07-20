18f7c44d26fac08d59210b545a969f78b7ea1d2e
Denis Yeresko <denis.yeresko@gmail.com>
2025-07-20 11:13:47 +0300

    test: add data display tests for result components

--- rs-react-app/src/components/Results/Result/Result.tsx
+++ rs-react-app/src/components/Results/Result/Result.tsx
@@ -2,11 +2,11 @@ import { Component } from 'react';
 import classes from './Result.module.css';
 
 interface IProps {
-  name: string;
-  status: string;
-  species: string;
-  gender: string;
-  imageUrl: string;
+  name?: string;
+  status?: string;
+  species?: string;
+  gender?: string;
+  imageUrl?: string;
 }
 
 class Result extends Component<IProps, object> {
@@ -16,24 +16,37 @@ class Result extends Component<IProps, object> {
         <img
           alt="Result image"
           className={classes.resultImage}
-          src={this.props.imageUrl}
+          src={
+            this.props.imageUrl
+              ? this.props.imageUrl
+              : 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/6c/No_image_3x4.svg/640px-No_image_3x4.svg.png'
+          }
+          data-testid="image"
         />
         <div className={classes.resultInfo}>
           <div className={classes.infoItem}>
             <span>Name:</span>
-            <span>{this.props.name}</span>
+            <span data-testid="name">
+              {this.props.name ? this.props.name : 'name is missing'}
+            </span>
           </div>
           <div className={classes.infoItem}>
             <span>Status:</span>
-            <span>{this.props.status}</span>
+            <span data-testid="status">
+              {this.props.status ? this.props.status : 'status is missing'}
+            </span>
           </div>
           <div className={classes.infoItem}>
             <span>Species:</span>
-            <span> {this.props.species}</span>
+            <span data-testid="species">
+              {this.props.species ? this.props.species : 'species is missing'}
+            </span>
           </div>
           <div className={classes.infoItem}>
             <span>Gender:</span>
-            <span>{this.props.gender}</span>
+            <span data-testid="gender">
+              {this.props.gender ? this.props.gender : 'gender is missing'}
+            </span>
           </div>
         </div>
       </div>
