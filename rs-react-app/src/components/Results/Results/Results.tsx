724980246deca89d78beff29fa8c770f707bd869
Denis Yeresko <denis.yeresko@gmail.com>
2025-07-27 11:44:48 +0300

    refactor: change Results class component to functional component

--- rs-react-app/src/components/Results/Results/Results.tsx
+++ rs-react-app/src/components/Results/Results/Results.tsx
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import { useEffect, useState } from 'react';
 import classes from './Results.module.css';
 import ErrorButton from '../ErrorButton.tsx';
 import logo from '../../../assets/react.svg';
@@ -26,65 +26,17 @@ export interface IResponse {
 interface IProps {
   searchResult?: string;
 }
+const baseApiQuery = 'https://rickandmortyapi.com/api/character/';
 
-interface IState {
-  characters: Character[];
-  apiQuery: string;
-  loading: boolean;
-  error: boolean;
-}
+function Results(props: IProps) {
+  const [characters, setCharacters] = useState<Character[]>([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+  const [apiQuery, setApiQuery] = useState('');
 
-class Results extends Component<IProps, IState> {
-  baseApiQuery: string;
-  constructor(props: IProps) {
-    super(props);
-    this.baseApiQuery = 'https://rickandmortyapi.com/api/character/';
-    this.state = { characters: [], apiQuery: '', error: false, loading: false };
-  }
-
-  setBaseApiQuery = () => {
-    this.setState({ loading: true });
-    this.setState(
-      {
-        apiQuery: this.baseApiQuery,
-      },
-      () => {
-        this.fetchResults();
-      }
-    );
-  };
-
-  setApiQuery = () => {
-    this.setState({ loading: true });
-    this.setState(
-      {
-        apiQuery: `${this.baseApiQuery}?name=${this.props.searchResult}`,
-      },
-      () => {
-        this.fetchResults();
-      }
-    );
-  };
-  setUrl = () => {
-    if (this.props.searchResult) {
-      this.setApiQuery();
-    } else {
-      this.setBaseApiQuery();
-    }
-  };
-  componentDidMount() {
-    this.setUrl();
-  }
-
-  componentDidUpdate(prevProps: IProps) {
-    if (prevProps.searchResult !== this.props.searchResult) {
-      this.setState({ loading: true });
-      this.setUrl();
-    }
-  }
-  fetchResults() {
-    this.setState({ error: false });
-    fetch(this.state.apiQuery)
+  useEffect(() => {
+    setError(false);
+    fetch(apiQuery)
       .then((response) => {
         if (!response.ok) {
           throw Error('Failed to fetch results.');
@@ -94,40 +46,47 @@ class Results extends Component<IProps, IState> {
       .then((data: IResponse) => {
         setTimeout(() => {
           if (data.results) {
-            this.setState({ loading: false, characters: data.results ?? [] });
+            setLoading(false);
+            setCharacters(data.results ?? []);
           }
         }, 200);
       })
       .catch(() => {
-        this.setState({ loading: false, error: true });
+        setError(true);
+        setLoading(false);
       });
-  }
+  }, [apiQuery]);
 
-  render() {
-    return (
+  useEffect(() => {
+    setLoading(true);
+    if (props.searchResult) {
+      setApiQuery(`${baseApiQuery}?name=${props.searchResult}`);
+    } else {
+      setApiQuery(baseApiQuery);
+    }
+  }, [props.searchResult]);
+
+  return (
+    <div>
       <div>
-        <div>
-          {this.state.loading && (
-            <img
-              data-testid="loader"
-              className="logo"
-              src={logo}
-              alt="Loading..."
-            />
-          )}
-          {this.state.error && (
-            <h2 data-testid="error" className={classes.header}>
-              Request did not succeed
-            </h2>
-          )}
-          {!this.state.error && !this.state.loading && (
-            <ResultList characters={this.state.characters} />
-          )}
-          <ErrorButton />
-        </div>
+        {loading && (
+          <img
+            data-testid="loader"
+            className="logo"
+            src={logo}
+            alt="Loading..."
+          />
+        )}
+        {error && (
+          <h2 data-testid="error" className={classes.header}>
+            Request did not succeed
+          </h2>
+        )}
+        {!error && !loading && <ResultList characters={characters} />}
+        <ErrorButton />
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default Results;
