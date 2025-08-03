905962bb49bce55b07c731bc91e27beac1ce88bd
Denis Yeresko <denis.yeresko@gmail.com>
2025-08-03 14:44:45 +0300

    feat: add download panel

--- rs-react-app/src/App.tsx
+++ rs-react-app/src/App.tsx
@@ -4,9 +4,12 @@ import MyErrorBoundary from './components/MyErrorBoundary.tsx';
 import useLocalStorage from '../hooks/useLocalStorage.tsx';
 import { useState } from 'react';
 import { Outlet, useMatch } from 'react-router-dom';
+import { useAppSelector } from '../hooks/reduxHooks.ts';
+import DownloadPanel from './components/DownloadPanel/DownloadPanel.tsx';
 
 function App() {
-  const [storageSearchResult] = useLocalStorage<string>('searchResult', '');
+  const [storageSearchResult] = useLocalStorage('searchResult', '');
+  const cards = useAppSelector((state) => state.cards.items);
   const [newPage] = useState('');
   const isPanelOpen = useMatch('/details/:id');
 
@@ -14,6 +17,7 @@ function App() {
     <div className={isPanelOpen ? 'App' : ''}>
       <MyErrorBoundary>
         <Results newPage={newPage} searchResult={storageSearchResult} />
+        {cards.length > 0 && <DownloadPanel />}
       </MyErrorBoundary>
       <Outlet />
     </div>
---
+++ rs-react-app/src/components/DownloadPanel/DownloadPanel.tsx
@@ -0,0 +1,55 @@
+import { useAppDispatch, useAppSelector } from '../../../hooks/reduxHooks.ts';
+import { clearCards } from '../../features/cards/cardsSlice.ts';
+import type { DetailedCharacter } from '../Panel/Panel.tsx';
+import classes from './DownloadPanel.module.css';
+import { useRef } from 'react';
+
+function charactersToCSV(characters: DetailedCharacter[]) {
+  const headers = [Object.keys(characters[0])].toString() + '\n';
+  const rows = characters
+    .map((character) => {
+      const values = Object.values(character);
+      return values.map((value) => {
+        if (typeof value === 'object') {
+          return Object.values(value)[0];
+        }
+        return value;
+      });
+    })
+    .join('\n');
+  return headers + rows;
+}
+
+function DownloadPanel() {
+  const cards = useAppSelector((state) => state.cards.items);
+  const dispatch = useAppDispatch();
+  const linkRef = useRef<HTMLAnchorElement>(null);
+
+  const handleUnselectAllClick = () => {
+    dispatch(clearCards());
+  };
+  const handleDownloadClick = () => {
+    const blob = new Blob([charactersToCSV(cards)], {
+      type: 'text/csv;charset=utf-8; ',
+    });
+    const url = URL.createObjectURL(blob);
+    const linkTag = linkRef.current;
+    if (linkTag) {
+      linkTag.href = url;
+      linkTag.download = `${cards.length}_items.csv`;
+      linkTag.click();
+    }
+  };
+  return (
+    <div className={classes.downloadPanel}>
+      <p>There are {cards.length} selected cards</p>
+      <div className={classes.panelButtons}>
+        <button onClick={handleDownloadClick}>Download</button>
+        <a href="#" ref={linkRef} hidden></a>
+        <button onClick={handleUnselectAllClick}>Unselect all</button>
+      </div>
+    </div>
+  );
+}
+
+export default DownloadPanel;
