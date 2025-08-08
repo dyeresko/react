48d2f160bc28c930e32045038ab69df6745a226e
Denis Yeresko <denis.yeresko@gmail.com>
2025-08-08 14:31:52 +0300

    fix: remove redundant set state

--- rs-react-app/src/layout/RootLayout.tsx
+++ rs-react-app/src/layout/RootLayout.tsx
@@ -2,12 +2,10 @@ import { Outlet } from 'react-router-dom';
 import Controls from '@components/Controls/index';
 import '@/App.css';
 import useLocalStorage from '@/hooks/useLocalStorage.tsx';
-import { useState } from 'react';
 import { useSearchParams } from 'react-router-dom';
 
 const RootLayout = () => {
   const [, setStorageSearchResult] = useLocalStorage('searchResult', '');
-  const [, setNewPage] = useState('');
   const [, setSearchParams] = useSearchParams();
 
   const onSearch = (value: string) => {
@@ -19,7 +17,6 @@ const RootLayout = () => {
     });
   };
   const onNewPage = (value: string) => {
-    setNewPage(value);
     const url = new URL(value);
     const page = url.searchParams.get('page');
     const name = url.searchParams.get('name');
