e6305d4b0bb2384116db8f0093fc0e70df846e0e
Denis Yeresko <denis.yeresko@gmail.com>
2025-08-18 00:53:25 +0300

    feat: add static request

--- rs-react-app/src/app/[locale]/characters/layout.tsx
+++ rs-react-app/src/app/[locale]/characters/layout.tsx
@@ -1,7 +1,7 @@
 'use client';
 import Controls from '@components/Controls/index';
 import '@/App.css';
-import { type FC, type ReactNode } from 'react';
+import { useEffect, useState, type FC, type ReactNode } from 'react';
 import Results from '@components/Results/Results/index';
 import MyErrorBoundary from '@components/MyErrorBoundary/index';
 import { useAppSelector } from '@/hooks/reduxHooks.ts';
@@ -9,6 +9,9 @@ import DownloadPanel from '@components/DownloadPanel/index';
 import { usePathname } from 'next/navigation';
 import { routing } from '@/i18n/routing';
 import { useTranslations } from 'next-intl';
+import type { Response } from '@/types/interfaces';
+import Image from 'next/image';
+import logo from '@/assets/react.svg';
 
 const App: FC<{ children: ReactNode }> = ({ children }) => {
   const cards = useAppSelector((state) => state.cards.items);
@@ -18,12 +21,39 @@ const App: FC<{ children: ReactNode }> = ({ children }) => {
     pathname.startsWith(`/${locale}/characters/details/`)
   );
   const t = useTranslations('Error Boundary');
+  const [initialResponse, setInitialResponse] = useState<Response | null>(null);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    async function fetchInitialData() {
+      const res = await fetch(`/api/results?page=1`);
+      const data: Response = await res.json();
+      setInitialResponse(data);
+      setIsLoading(false);
+    }
+    fetchInitialData();
+  }, []);
+
+  if (isLoading)
+    return (
+      <div>
+        <Image
+          width={300}
+          height={300}
+          data-testid="loader"
+          className="logo"
+          src={logo}
+          alt={`${t('alt-loading')}...`}
+        />
+      </div>
+    );
+
   return (
     <>
       <Controls />
       <div className={isPanelOpen ? 'App' : ''}>
         <MyErrorBoundary t={t}>
-          <Results />
+          <Results initPage={1} response={initialResponse} />
           {cards.length > 0 && <DownloadPanel />}
         </MyErrorBoundary>
         {children}
--- rs-react-app/src/components/Results/Results/index.tsx
+++ rs-react-app/src/components/Results/Results/index.tsx
@@ -1,11 +1,11 @@
 'use client';
-import { useEffect, useState, type FC } from 'react';
+import { useEffect, type FC } from 'react';
 import classes from '@components/Results/Results/Results.module.css';
 import ErrorButton from '@components/Results/ErrorButton';
 import logo from '@/assets/react.svg';
 import ResultList from '@components/Results/ResultList/index';
 import { useSearchParams } from 'next/navigation';
-import type { Character } from '@/types/interfaces';
+import type { Response } from '@/types/interfaces';
 import useUpdateSearchParams from '@/hooks/useUpdateSearchParams';
 import { useGetResultsQuery } from '@/app/[locale]/lib/services/api';
 import useLocalStorage from '@/hooks/useLocalStorage';
@@ -14,34 +14,38 @@ import { setPagination } from '@/app/[locale]/lib/features/pagination/pagination
 import Image from 'next/image';
 import { useTranslations } from 'next-intl';
 
-const Results: FC = () => {
+const Results: FC<{ initPage: number; response: Response | null }> = ({
+  initPage,
+  response,
+}) => {
   const t = useTranslations('Result');
-  const [characters, setCharacters] = useState<Character[]>([]);
   const dispatch = useAppDispatch();
   const searchParams = useSearchParams();
   const updateSearchParams = useUpdateSearchParams();
   const page = Number(searchParams.get('page') || '1');
   const name = searchParams.get('name') || '';
   const [, setStorageSearchResult] = useLocalStorage('searchResult', '');
-
-  const { data, error, isFetching, refetch } = useGetResultsQuery({
-    page,
-    name,
-  });
+  const { data, error, isFetching, refetch } = useGetResultsQuery(
+    {
+      page,
+      name,
+    },
+    { skip: page === initPage }
+  );
+  const res = page === initPage ? response : data || null;
 
   useEffect(() => {
     updateSearchParams({ page: String(page), name: name });
     setStorageSearchResult(name);
-  }, [page, name]);
-
-  useEffect(() => {
-    if (data?.results) {
-      setCharacters(data.results);
-    }
     if (data?.info) {
       dispatch(setPagination(data.info));
     }
-  }, [data]);
+    if (page === 1) {
+      if (res?.info) {
+        dispatch(setPagination(res?.info));
+      }
+    }
+  }, [page, name, initPage, res, data]);
 
   const handleRefreshClick = () => {
     refetch();
@@ -73,7 +77,7 @@ const Results: FC = () => {
     <div>
       <button onClick={handleRefreshClick}>{t('refresh')}</button>
       <div>
-        <ResultList characters={characters} />
+        <ResultList characters={res?.results} />
         <ErrorButton />
       </div>
     </div>
