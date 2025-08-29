cb7743903d2e9ce80e27de15c82424f8f065a87b
Denis Yeresko <denis.yeresko@gmail.com>
2025-08-29 16:09:05 +0300

    feat: add co2 and co2_per_capita columns

--- rs-react-app/src/components/CountriesTable/index.tsx
+++ rs-react-app/src/components/CountriesTable/index.tsx
@@ -18,15 +18,21 @@ const CountriesTable: FC = () => {
             <th scope="col">Year</th>
             <th scope="col">Population</th>
             <th scope="col">ISO code</th>
+            <th scope="col">co2</th>
+            <th scope="col">co2_per_capita</th>
           </tr>
         </thead>
         <tbody>
           {Object.entries(data).map(([key, value]) => (
-            <tr key={key.toLowerCase()}>
+            <tr key={key}>
               <th scope="row">{key}</th>
               <td>{value.data[value.data.length - 1].year}</td>
               <td>{value.data[value.data.length - 1].population ?? 'N/A'}</td>
               <td>{value.iso_code ?? 'N/A'}</td>
+              <td>{value.data[value.data.length - 1].co2 ?? 'N/A'}</td>
+              <td>
+                {value.data[value.data.length - 1].co2_per_capita ?? 'N/A'}
+              </td>
             </tr>
           ))}
         </tbody>
